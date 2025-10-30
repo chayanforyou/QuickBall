2bbd929ebfc0062d5f3562e5029d487fdf4ef612
Chayan Mistry <chayanmistrry@gmail.com>
2025-10-30 08:43:21 +0600

    Enable dynamic colors and dark mode

--- app/src/main/java/io/github/chayanforyou/quickball/ui/fragments/OptimizationRemoverSheet.kt
+++ app/src/main/java/io/github/chayanforyou/quickball/ui/fragments/OptimizationRemoverSheet.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.elevation.SurfaceColors
 import io.github.chayanforyou.quickball.databinding.SheetDokiTutorialBinding
@@ -29,7 +24,7 @@ class OptimizationRemoverSheet : BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = SheetDokiTutorialBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -37,25 +32,10 @@ class OptimizationRemoverSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bottomSheetDialog = dialog as BottomSheetDialog
-        val behavior = bottomSheetDialog.behavior
-        
-        // Set initial peek height to 85% of screen height
-        behavior.peekHeight = (resources.displayMetrics.heightPixels * 0.85).toInt()
-
         binding.dokiView.apply {
             setButtonsVisibility(false)
-
-            val appbar = findViewById<AppBarLayout>(dev.doubledot.doki.R.id.appbar)
-
-            (appbar.getChildAt(0) as CollapsingToolbarLayout).setContentScrimColor(
-                ContextCompat.getColor(context, android.R.color.transparent)
-            )
-
             headerBackgroundColor = SurfaceColors.SURFACE_1.getColor(context)
-
-            findViewById<View>(dev.doubledot.doki.R.id.divider3).visibility = View.GONE
-
+            findViewById<View?>(dev.doubledot.doki.R.id.divider3)?.visibility = View.GONE
             loadContent(appName = "Quick Ball")
         }
     }
