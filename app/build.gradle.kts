27e9e8da10830e370659352629ae1269570687d4
Chayan Mistry <chayanmistrry@gmail.com>
2026-04-28 17:28:36 +0600

    Fixed high-risk detection issue caused by accessibility permission.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.chayanforyou.quickball"
         minSdk = 21
         targetSdk = 36
-        versionCode = 14
-        versionName = "4.2.1"
+        versionCode = 15
+        versionName = "4.3.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
